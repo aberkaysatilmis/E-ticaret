cb6aab97f083419df0b77ca47b1bfe9360ebf8d4
ZypraX <aberkaysatilmis@gmail.com>
2023-04-20 14:03:00 +0300

    Project Updating V2.1

--- UrunKMVCWebUI/Entity/Category.cs
+++ UrunKMVCWebUI/Entity/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +10,14 @@ namespace UrunKMVCWebUI.Entity
     public class Category
     {
         public int Id { get; set; }
+
+
+        //Data annotations.
+        [DisplayName("Kategori Adı")]
+        [StringLength(maximumLength:20, ErrorMessage ="En fazla 20 karakter girilebilir.")]
         public string Name { get; set; }
+
+        [DisplayName("Açıklama")]
         public string Description { get; set; }
 
         public  List<Product> Products { get; set; }
