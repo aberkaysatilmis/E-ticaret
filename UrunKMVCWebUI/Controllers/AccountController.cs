72f28bc1656f8f5f718dfbcaeeec6e496bdf0d5f
ZypraX <aberkaysatilmis@gmail.com>
2023-05-02 20:09:11 +0300

    Project Updating v2.2

---
+++ UrunKMVCWebUI/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UrunKMVCWebUI.Identity;
+using UrunKMVCWebUI.Models;
+
+namespace UrunKMVCWebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private UserManager<ApplicationUser> UserManager;
+        private RoleManager<ApplicationRole> RoleManager;
+
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            UserManager = new UserManager<ApplicationUser>(userStore);
+
+            var roleStore = new RoleStore<ApplicationRole>(new IdentityDataContext());
+            RoleManager = new RoleManager<ApplicationRole>(roleStore);
+        }
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Kayıt işlemleri
+
+                ApplicationUser user = new "();
+                user.Name = model.Name;
+                user.Surname = model.SurName;
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+
+                IdentityResult result = UserManager.Create(user,model.Password);
+
+                if (result.Succeeded)
+                {
+                    //Kullanıcı oluştu ve kullanıcıyı bir role atayabilirsiniz.
+                    if(RoleManager.RoleExists("User"))
+                    {
+                        UserManager.AddToRole(user.Id, "User");
+                    }
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("RegisterUserError","Kullanıcı oluşturma hatası.");
+                }
+
+            }
+
+            return View(model);
+        }
+    }
+}
