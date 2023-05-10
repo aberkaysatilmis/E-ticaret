7295eef3c3f411b192e949e720a57ab32284d458
esk <aberkaysatilmis@gmail.com>
2023-05-10 16:58:25 +0300

    Project Added v2.7

--- UrunKMVCWebUI/Controllers/CartController.cs
+++ UrunKMVCWebUI/Controllers/CartController.cs
@@ -3,15 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UrunKMVCWebUI.Entity;
+using UrunKMVCWebUI.Models;
 
 namespace UrunKMVCWebUI.Controllers
 {
     public class CartController : Controller
     {
+
+        private DataContext db = new DataContext();
         // GET: Cart
         public ActionResult Index()
         {
-            return View();
+            return View(GetCart());
         }
+
+        public ActionResult AddToCart(int Id)
+        {
+            var product =db.Products.FirstOrDefault(i=>i.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().AddProduct(product, 1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            var cart = (Cart)Session["Cart"];   
+            //Session : Her kullanıcıya özel oluşturulan depodur.
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"]=cart;
+            }
+            return cart;
+        }
+
     }
 }
