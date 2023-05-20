fd3d774380b59fc3a5bea73823467b1f8f3a0b2a
Ahmet <aberkaysatilmis@gmail.com>
2023-05-20 15:53:00 +0300

    v3.0

---
+++ UrunKMVCWebUI/Entity/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UrunKMVCWebUI.Entity
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("dataConnection")
+        {
+            Database.SetInitializer(new DataInitializer());
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderLine> OrderLines  { get; set; }
+
+    }
+
+}
